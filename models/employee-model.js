af850f1c651617a9ca50fa389066bd651ea506c2
Joshua Zuo <neveragain8209@yahoo.com>
2018-05-28 19:04:58 -0500

    models changed

--- models/employee-model.js
+++ models/employee-model.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema({
   name: {type: String, required: true},
   sunday: {type: Number},
-  monday: {type: String},
-  tuesday: {type: String},
-  wednesday: {type: String},
-  thursday: {type: String},
-  friday: {type: String},
-  saturday: {type: String},
+  monday: {type: Number},
+  tuesday: {type: Number},
+  wednesday: {type: Number},
+  thursday: {type: Number},
+  friday: {type: Number},
+  saturday: {type: Number},
 });
