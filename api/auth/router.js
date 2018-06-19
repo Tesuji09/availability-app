3e55c071f44afba657d4fccf094c8ecdc69cd5bb
Joshua Zuo <neveragain8209@yahoo.com>
2018-06-19 08:57:14 -0500

    Removing unnecessary dotenv calls

--- api/auth/router.js
+++ api/auth/router.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
--- api/routes/user-router.js
+++ api/routes/user-router.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
 
 const bodyParser = require('body-parser');
 
